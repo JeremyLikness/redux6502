9eb7db710cc2eb26060f187bd014f4c34eefcc9d
angular-cli <angular-cli@angular.io>
2016-12-20 10:17:48 -0500

    chore: initial commit from angular-cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Redux6502Page } from './app.po';
+
+describe('redux6502 App', function() {
+  let page: Redux6502Page;
+
+  beforeEach(() => {
+    page = new Redux6502Page();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
