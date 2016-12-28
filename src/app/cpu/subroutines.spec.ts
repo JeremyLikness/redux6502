eaf431f036383281b02423c2f4fb1bed4cdea17a
Jeremy Likness <jeremy@jeremylikness.com>
2016-12-28 11:25:56 -0500

    Add subroutine for BEQ

--- src/app/cpu/subroutines.spec.ts
+++ src/app/cpu/subroutines.spec.ts
@@ -7,6 +7,11 @@ import { createStore, Store } from 'redux';
 
 import { TestBed } from '@angular/core/testing';
 
+const perf = (cpu: Cpu) => {
+    console.log(`Ran ${cpu.stats.instructionCount} ops in ${cpu.stats.ellapsedMilliseconds}ms` +
+            ` at ${cpu.stats.instructionsPerSecond} ips`);
+};
+
 describe('sample programs', () => {
 
     let store: Store<Cpu> = null;
@@ -33,7 +38,36 @@ describe('sample programs', () => {
             store.dispatch(cpuSetPC(0xC000));
             store.dispatch(cpuStart());
             store.dispatch(cpuRun(6));
-            expect(store.getState().memory[0xC009]).toBe(0x7F);
+            let cpu = store.getState();
+            expect(cpu.memory[0xC009]).toBe(0x7F);
+            perf(cpu);
+        });
+    });
+
+    describe('LDA and BEQ', () => {
+/*
+    C000: LDX #$00 
+    C002: LDA $C00C,X
+    C005: BEQ $C00B 
+    C007: INX 
+    C008: JMP $C002
+    C00B: 0X00 
+    C00C: 01 02 03 04 05 00 
+*/
+        it('loops until a zero value is encountered', () => {
+            store.dispatch(
+                cpuPoke(
+                    0xC000,
+                    [0xA2, 0x00, 0xBD, 0x0C, 0xC0, 0xF0, 0x05, 0xE8, 0x4C, 0x02, 0xC0, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05]
+                )
+            );
+            store.dispatch(cpuSetPC(0xC000));
+            store.dispatch(cpuStart());
+            store.dispatch(cpuRun(255));
+            let cpu = store.getState();
+            expect(cpu.rPC).toBe(0xC00C);
+            expect(cpu.rX).toBe(0x05);
+            perf(cpu);
         });
     });
 });
